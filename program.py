97a7132b763465c303dc19ae5f3c24897c1ae54f
atulspatil <atulspatil1012@gmail.com>
2021-05-26 08:23:29 +0530

    second commit

--- program.py
+++ program.py
@@ -4,7 +4,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def jenkins_second():
-    return "This is second Jenkins project !"
+    return "This is second Jenkins project ! Now adding new line..."
 
 
 if __name__ == "__main__":
